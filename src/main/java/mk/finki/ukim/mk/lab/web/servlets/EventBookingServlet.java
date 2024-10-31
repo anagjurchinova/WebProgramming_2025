fa9c84cd7fc2f8cb887b352edfdf5da614eda7eb
anagjurchinova <a.gjurcinova9@gmail.com>
2024-10-31 18:50:10 +0100

    wp lab

--- src/main/java/mk/finki/ukim/mk/lab/web/servlets/EventBookingServlet.java
+++ src/main/java/mk/finki/ukim/mk/lab/web/servlets/EventBookingServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mk.finki.ukim.mk.lab.model.EventBooking;
 import mk.finki.ukim.mk.lab.service.EventBookingService;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -35,6 +36,37 @@ public class EventBookingServlet extends HttpServlet  {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String nameSearched = req.getParameter("searchedByName");
+        String ratingSearched = req.getParameter("searchedByRating");
+
+        if (nameSearched != null && !nameSearched.isEmpty()) {
+            req.getSession().setAttribute("searchedName", nameSearched);
+        }
+        if (ratingSearched != null && !ratingSearched.isEmpty()) {
+            req.getSession().setAttribute("searchedRating", ratingSearched);
+        }
+
+        String eventName = req.getParameter("eventName");
+        String attendee = req.getParameter("attendeeName");
+        int numTickets = 0;
+
+        if (req.getParameter("numTickets") != null && !req.getParameter("numTickets").isEmpty()) {
+            numTickets = Integer.parseInt(req.getParameter("numTickets"));
+        }
+
+        if (eventName != null && attendee != null && numTickets > 0) {
+            String clientIP = req.getRemoteAddr();
+            try {
+                EventBooking booking = eventBookingService.placeBooking(eventName, attendee, clientIP, numTickets);
+                req.getSession().setAttribute("eventBooking", booking);
+                resp.sendRedirect("/eventBooking");
+                return; // zatoa sto se naogja inside if statement, vo sprotivno server-side ke prodolzi da go execute ostanatiot kod
+
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        resp.sendRedirect("/");
     }
 }
